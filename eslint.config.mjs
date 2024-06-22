70e2e56a4790343dee4ea307f7954988b6d948ee
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-06-23 00:05:43 +0900

    Update eslint.config.mjs
    accomodate -> accommodate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ export default [
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
-            'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
+            'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accommodate await lines)
             'no-constant-condition': 'off', // allow constant conditions
             'no-empty': 'off', // allow empty blocks
             'no-inner-declarations': 'off', // allow function declarations anywhere
